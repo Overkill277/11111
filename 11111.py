44f5622939cace9e5f7109a58f9f06237d4248b9
Overkill277 <vasiliy@DESKTOP-KNJFFPB.localdomain>
2020-01-12 23:12:57 +0300

    мой первый комментарий

---
+++ 11111.py
@@ -0,0 +1,41 @@
+def hangman(word):
+	wrong = 0
+	stages = ["",
+			 "________ 		",
+			 "|       |		",
+			 "|       |		",
+			 "|       0		",
+			 "|      /|\	",
+			 "|      / \	",
+			 "|             "]
+	rletters = list(word)
+	board = ["_"] * len(word)
+	win = False
+	print("START")
+	while wrong < len(stages) - 1:
+		print("\n")
+		msg = "Буква?"
+		char = input(msg)
+		if char in rletters:
+			cind = rletters.index(char)
+			board[cind] = char
+			rletters[cind] = '$'
+		else:
+			wrong += 1
+		print((" ".join(board)))
+		e = wrong + 1
+		print("\n".join(stages[0: e]))
+		if "_" not in board:
+			print("Вы выиграли! Было загадано слово: ")
+			print(" ".join(board))
+			win = True
+			break
+	if not win:
+		print("\n".join(stages[0: wrong]))
+		print ("Вы проиграли! Было загадано слово:{}.".format(word))
+
+import random
+
+words = ["десять","восемь","девять","тысяча","полтос"]
+n = random.randint(0,4)
+hangman(words[n])
